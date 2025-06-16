a91767b0ac92a3a82651ca809e26ee0da9b4c0e7
Givanilson Pereira <74155135+givanilsonpereira@users.noreply.github.com>
2025-06-16 16:35:08 +0000

    Adiciona estilo para a lista de participantes nas atividades

--- src/static/app.js
+++ src/static/app.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const participantsList =
           details.participants.length > 0
-            ? `<ul>${details.participants.map((participant) => `<li>${participant}</li>`).join("")}</ul>`
+            ? `<ul class="participants-list">${details.participants.map((participant) => `<li>${participant}</li>`).join("")}</ul>`
             : "<p>No participants yet</p>";
 
         activityCard.innerHTML = `
